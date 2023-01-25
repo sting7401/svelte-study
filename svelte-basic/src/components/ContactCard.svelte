2099edf26854920b7019e1b84db9319e1397c5c7
sting7401 <sting7401@naver.com>
2023-01-25 19:17:09 +0900

    기초 연습 다시 시작

---
+++ svelte-basic/src/components/ContactCard.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let inputList1;
+	export let inputList2;
+	export let inputList3;
+	export let inputList4;
+
+	const initName = inputList1;
+</script>
+
+<div>
+	<img src="" alt="" />
+	<div>
+		{inputList1} / {initName}
+	</div>
+	<div>
+		{inputList2}
+	</div>
+	<div>
+		{inputList3}
+	</div>
+	<div>
+		{inputList4}
+	</div>
+</div>
+
+<style>
+	/* your styles go here */
+</style>
---
+++ svelte-basic/src/routes/basic2/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+let number = 0;
+
+$: numberCalc = number < 0 ? 0 : number;
+
+const increment = () => {
+	number += 1;
+};
+
+const decrement = () => {
+	number -= 1;
+};
+</script>
+
+<style>
+</style>
+
+<p>{number}</p>
+<p>{numberCalc}</p>
+
+<button type="button" on:click|preventDefault="{increment}">+</button>
+<button type="button" on:click|preventDefault="{decrement}">-</button>
+
+<button type="button" on:click|preventDefault="{() => (number += 1)}">+</button>
+<button type="button" on:click|preventDefault="{() => (number -= 1)}">-</button>
