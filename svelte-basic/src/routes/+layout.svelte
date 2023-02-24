30c84f8b5727dfa68bb770d9921bbf731b8cff40
sting7401 <sting7401@naver.com>
2023-02-24 16:28:47 +0900

    bb

--- svelte-basic/src/routes/+layout.svelte
+++ svelte-basic/src/routes/+layout.svelte
@@ -1,3 +1,24 @@
+<script>
+	import '../input.css';
+</script>
+
+<style>
+	li {
+		margin-right: 1rem;
+		list-style-type: none;
+	}
+</style>
+
 <a href="/step02">step02</a>
+
+<nav class="flex">
+	{#each Array(12) as _, index (index)}
+		<li>
+			<a href="basic{index}" class="text-3xl font-bold underline">
+				{index}
+			</a>
+		</li>
+	{/each}
+</nav>
 
 <slot><!-- optional fallback --></slot>
---
+++ svelte-basic/src/routes/basic11/Slot.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let slotText;
+
+	console.log(slotText);
+</script>
+
+<style>
+</style>
+
+<div class="bg-slate-400">
+	<slot><!-- optional fallback --></slot>
+
+	<slot name="slotText"><!-- optional fallback --> 1112</slot>
+</div>
