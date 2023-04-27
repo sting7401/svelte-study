ccc5dec0e3b58fb9f0562f5b9f53aec3dec230e4
sting7401 <sting7401@naver.com>
2023-04-27 16:33:49 +0900

    code

---
+++ svelte-basic/src/routes/(kit)/tutorial/docs/[...slug]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	console.log($page.params.slug);
+
+	const slugArr: string[] = $page.params.slug.split('/');
+</script>
+
+<style>
+</style>
+
+<p>Docs page</p>
+<p>Catch All Routesd</p>
+<p>params.slug로 url 체크</p>
+
+{#if slugArr.length === 1}
+	<p>{slugArr[0]}</p>
+{:else if slugArr.length === 2}
+	<p>
+		{slugArr[0]}/{slugArr[1]}
+	</p>
+{/if}
---
+++ svelte-basic/src/routes/(kit)/tutorial/products/[productNumber]/reviews/[reviewId]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+	const { productNumber, reviewId } = $page.params;
+</script>
+
+<style>
+</style>
+
+<p>{productNumber} {reviewId}</p>
