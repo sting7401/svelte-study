015d45502796f9dbf24e0d51025d4e9e479792af
sting7401 <sting7401@naver.com>
2023-04-27 09:44:54 +0900

    study

--- svelte-basic/src/routes/+layout.svelte
+++ svelte-basic/src/routes/+layout.svelte
@@ -9,13 +9,24 @@
 	}
 </style>
 
-<a href="/step02">step02</a>
-
-<nav class="flex">
+<nav class="flex hover:">
 	{#each Array(12) as _, index (index)}
 		<li>
-			<a href="basic{index}" class="text-3xl font-bold underline">
+			<a
+				href="/start/basic{index}"
+				class="text-sm font-bold underline transition animate-bounce hover:text-lg"
+			>
 				{index}
+			</a>
+		</li>
+	{/each}
+	{#each Array(2) as _, i (i)}
+		<li>
+			<a
+				href="/start/step0{i + 1}"
+				class="text-sm font-bold underline transition animate-bounce hover:text-lg"
+			>
+				{i + 1}
 			</a>
 		</li>
 	{/each}
---
+++ svelte-holo/src/lib/comments.ts
@@ -0,0 +1,10 @@
+type commentList = {
+	id: number;
+	text: string;
+};
+
+export const comments: commentList[] = [
+	{ id: 1, text: '댓글' },
+	{ id: 2, text: '댓글' },
+	{ id: 3, text: '댓글' },
+];
