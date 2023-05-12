8b44ad0954e625e1ff6e8458313033b4649ad4e6
sting7401 <sting7401@naver.com>
2023-05-12 16:58:02 +0900

    boilerplate 수정

--- boilerplate/tailwind.config.cjs
+++ boilerplate/tailwind.config.cjs
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{html,js}'],
+	content: ['./index.html', './src/**/*.{js,svelte}', './src/*.{js,svelte}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
+	darkMode: false, // or 'media' or 'class'
 };
