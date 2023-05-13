0b08573371d9251328b9d2dbc396d8e9f74f367a
sting7401 <sting7401@naver.com>
2023-05-13 14:36:55 +0900

    todo 초기 작업

---
+++ svelte-course/src/routes/(basic)/todoList/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { TodoType } from '$lib/types/Todo';
+	import TodoList from '$lib/components/TodoList.svelte';
+	import { v4 as uuidv4 } from 'uuid';
+
+	let todoWrap: TodoType[] = [];
+</script>
+
+<TodoList bind:todoWrap on:addTodo={() => {}} />
+
+<style>
+</style>
