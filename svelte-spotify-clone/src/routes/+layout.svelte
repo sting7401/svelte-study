072058a95f809edd374662ca118996579e5144c1
sting7401 <sting7401@naver.com>
2023-06-13 17:24:31 +0900

    초기 설정

---
+++ svelte-spotify-clone/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import 'modern-normalize/modern-normalize.css';
+
+	// @ts-check
+	/**
+	 * @type {string} text
+	 */
+	let text = 1200;
+</script>
+
+{text}
+
+<slot><!-- optional fallback --></slot>
---
+++ svelte-spotify-clone/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="scss">
+
+   
+	h1 {
+		font-size: functions.rem(36);
+
+        @include breakpoint.up('lg') {
+            color: blue;
+        }
+	}
+</style>
